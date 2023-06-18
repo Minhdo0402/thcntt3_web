38312f382e0da82ff2597acc5f430c033e9d1e1b
Nguyen Trung Kien <nguyenkien11202000@gmail.com>
2023-06-18 17:18:49 +0700

    Upload All Project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+// Router
+import { Route, Switch } from 'react-router-dom';
+// Components
+import Home from './components/Home';
+import AddEdit from './components/AddEdit';
+import View from './components/View';
+import About from './components/About';
+import Faq from './components/Faq';
+import NotFound from './components/NotFound';
+// Toast
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+// Bootstrap
+import 'bootstrap/dist/css/bootstrap.css';
+import { Container } from 'react-bootstrap';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Container className='d-flex flex-column align-items-center bg-light p-5 mt-5'>
+        <ToastContainer
+          position='top-center'
+          autoClose={1500}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          // hiển thị icon message progressbar theo chiều từ phải sang trái
+          rtl={false}
+          theme="light"
+        />
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='/detail/:id' component={View} />
+          <Route path='/add' component={AddEdit} />
+          <Route path='/edit/:id' component={AddEdit} />
+          <Route path='/about' component={About} />
+          <Route path='/faq' component={Faq} />
+          <Route component={NotFound} />
+        </Switch>
+      </Container>
+    </>
   );
 }
 
